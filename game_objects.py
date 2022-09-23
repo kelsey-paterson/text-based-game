8d7141b31519933daeda2c2e8bf25448929f763c
Kelsey Paterson <kelseypaterson72@gmail.com>
2022-09-24 11:53:45 +1200

    Add game states class and create game_state object

--- game_objects.py
+++ game_objects.py
@@ -1,1 +1,14 @@
-# Module containing game objects and thier methods
+## Module defining game objects and their methods
+
+
+# Define game states class
+# Controls what is displayed in game main while loop
+class GameState:
+
+    def __init__(self):
+        self.is_running = True
+
+
+
+# Create game states object
+game_state = GameState()
