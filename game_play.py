c22694199cc2deae0a683dd954178d9ae714e863
Kelsey Paterson <kelseypaterson72@gmail.com>
2022-11-10 21:01:59 +1300

    Get chest sequence working and item generation working

--- game_play.py
+++ game_play.py
@@ -69,18 +69,20 @@ def prepare_game():
   # Clear screen
   gdi.window.fill((0, 0, 0))
 
+
 def main_game(event):
-  gdi.display_main_game_screen()
   if go.game_state.choosing_path:
     choose_path(event)
   elif go.game_state.view_room:
     view_room()
   elif go.game_state.room_transition:
-    handle_enter(event)
+    room_handle_enter(event)
   elif go.game_state.combat:
     combat(event)
   elif go.game_state.chest:
-    open_chest()
+    open_chest(event)
+  elif go.game_state.chest_transition:
+    chest_handle_enter(event)
 
 def choose_path(event):
   # Hint_text.text = ''
@@ -110,8 +112,8 @@ def combat():
 
 def chest_generate_item_category():
   '''Randomly chooses item category to be in chest'''
-  chest_options = gd.chest_chances.keys()
-  chest_chances = gd.chest_chances.values()
+  chest_options = list(gd.chest_chances.keys())
+  chest_chances = list(gd.chest_chances.values())
   return helper.getRandom(chest_options, chest_chances)
 
 def chest_generate_item_type(item_category):
@@ -121,14 +123,14 @@ def chest_generate_item_type(item_category):
 
   if item_category == 'sword' or item_category == 'shield':
     type_chances = get_item_chances(player_level, item_category)
-    types = gd.chest_chance_dict_2[item_category].keys()
+    types = list(gd.chest_chance_dict[item_category].keys())
     item = helper.getRandom(types, type_chances)
 
   elif item_category == 'potion':
-    strength_chances = get_item_chances(player_level, item_category)
-    potion_types = gd.chest_chance_dict_2[item_category]
+    potion_types = gd.chest_chance_dict[item_category]['types']
     type = random.choice(potion_types)
-    strength_types = gd.chest_chance_dict_2[item_category].potion_strength.values()
+    strength_types = list(gd.chest_chance_dict[item_category]['potion_strength'].keys())
+    strength_chances = list(gd.chest_chance_dict[item_category]['potion_strength'].values())
     strength = helper.getRandom(strength_types, strength_chances)
     item = strength + ' ' + type
   
@@ -140,31 +142,28 @@ def get_item_chances(player_level, item_category):
   chances = []
   level_index = int(player_level) - 1
   if item_category == 'sword' or item_category == 'shield':
-    for item in gd.chest_chance_dict_2[item_category]:
-      chances.push(item.value()[level_index]*go.player.luck)
-  elif item_category == 'potion':
-    chances = gd.chest_chance_dict_2[item_category].potion_strength.values() * go.player.luck
+    for item in gd.chest_chance_dict[item_category].values():
+      chances.append(item[level_index]*go.player.luck)
 
   return chances
 
 def chest_generate_open_text(item_name):
-    # window.fill((0, 0, 0))
-  gdi.Text_1.text = 'You found a {} {}........'.format(item_name)
-  gdi.Text_2.text = '(Press Enter)'
+  gdi.window.fill((0, 0, 0))
+  gdi.Game_Text_1.text = 'You found a {} ........'.format(item_name)
+  gdi.Game_Text_2.text = '(Press Enter)'
+
 
 def open_chest(event):
   '''Player opens chest'''
   # TODO: Luck variable should factor into result.
   item_category = chest_generate_item_category()
-  item_type = chest_generate_item_type()
-  item_name = item_type + item_category
+  item_type = chest_generate_item_type(item_category)
+  item_name = item_type + ' ' + item_category
   chest_generate_open_text(item_name)
+  go.player.inventory[item_name] = go.Item(item_category, item_type, item_name)
+  go.game_state.chest_transition = True  
+  go.game_state.chest = False
 
-  if event.type == pg.KEYDOWN:
-    if event.key == (pg.K_RETURN):
-      go.player.inventory[item_name] = go.Item(item_name)
-      go.game_state.chest = False
-      go.game_state.choosing_path = True
   # go.player.check_inventory(item_category, item_type)
   # if chest_contains == 'shield' and go.player.first_shield:
   #     gdi.Hint_text.text = 'GAME HINT: / A shield reduces or blocks part of an enemy attack'
@@ -176,22 +175,32 @@ def open_chest(event):
   #     Hint_text.text = 'GAME HINT: / You can use a potion during combat to temporarily increase your stats'
   #     player.first_potion = False
   # if item_name in player.inventory.keys():
-  #     Text_1.text += gt.have_object_text
+  #     Game_Text_1.text += gt.have_object_text
   # else:
   #     player.inventory[item_name] = Item(item_name)
   # self.chest_opened = True
 
-def handle_enter(event):
+def room_handle_enter(event):
   '''Handle enter from room content outcome'''
   if event.type == pg.KEYDOWN:
     if event.key == (pg.K_RETURN):
       if go.room_dict[go.player.location].content == 'chest':
         go.game_state.chest = True
+        go.game_state.room_transition = False
       elif go.room_dict[go.player.location].content == 'enemy':
         go.game_state.combat = True
+        go.game_state.room_transition = False
       else:
         # Room is empty
         go.game_state.choosing_path = True
+        go.game_state.room_transition = False
+
+def chest_handle_enter(event):
+  '''Handle enter after chest opened'''
+
+  if event.type == pg.KEYDOWN:
+    if event.key == (pg.K_RETURN):
+      go.game_state.choosing_path = True
 
 
 
